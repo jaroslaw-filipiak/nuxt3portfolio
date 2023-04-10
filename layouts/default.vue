c9a3c99e9ef4ed2beccf268f603e8a508d1711ec
jaroslaw-filipiak <info@j-filipiak.pl>
2023-04-10 11:33:06 +0200

    add hotjar to head in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,6 +30,27 @@
       header.classList.toggle('top-bar__sticky', window.scrollY > 0);
     });
   });
+
+  useHead({
+    script: [
+      {
+        hotjar: `(function (h, o, t, j, a, r) {
+  h.hj =
+    h.hj ||
+    function () {
+      (h.hj.q = h.hj.q || []).push(arguments);
+    };
+  h._hjSettings = { hjid: 3304441, hjsv: 6 };
+  a = o.getElementsByTagName('head')[0];
+  r = o.createElement('script');
+  r.async = 1;
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+`,
+      },
+    ],
+  });
 </script>
 
 <style lang="scss">
