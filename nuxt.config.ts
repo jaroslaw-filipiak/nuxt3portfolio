3c574f4bd3cb5ae13f152d296235436e16eecfca
jaroslaw-filipiak <info@j-filipiak.pl>
2023-11-25 16:31:39 +0100

    favicon as png

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.ico' }],
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' }],
     },
   },
 
