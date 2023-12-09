9afa2a2448624b31dfc01f01a87f2f76ef1b6112
jaroslaw-filipiak <info@j-filipiak.pl>
2023-12-09 22:44:31 +0100

    add terms page

---
+++ pages/regulamin.vue
@@ -0,0 +1,363 @@
+<template>
+  <div
+    class="page-content pt-40 pb-20 border border-dark-3 text-dark-3 pl-8 pr-8"
+    ref="main"
+  >
+    <div class="container">
+      <div
+        data-v-11b4eedd=""
+        data-v-164ff72a=""
+        class="results__agreement-preview"
+      >
+        <h1 class="text-5xl mb-10 max-w-[60%]">Regulamin</h1>
+        <h2 class="pb-3 lg:max-w-[60%]">
+          <strong
+            >Witamy w j-filipiak.pl! Niniejszy regulamin określa zasady
+            korzystania ze Strony internetowej INCREMENT POLAND Jarosław
+            Filipiak, dostępnej pod adresem https://www.j-filipiak.pl.
+            Użytkownik wchodząc na Stronę internetową, akceptuje niniejsze
+            warunki. W przypadku braku akceptacji niniejszych warunków
+            Użytkownik powinien opuścić Stronę j-filipiak.pl.</strong
+          >
+        </h2>
+
+        <h2 class="pb-12">Pliki cookie:</h2>
+        <p>
+          Na tej Stronie internetowej wykorzystujemy pliki cookie, aby
+          spersonalizować doświadczenia online naszych odbiorców. Wchodząc na
+          Stronę j-filipiak.pl, wyrażasz zgodę na używanie wymaganych plików
+          cookie.
+        </p>
+        <p>
+          Plik cookie to plik tekstowy, który jest umieszczany przez serwer
+          strony internetowej na dysku twardym komputera Użytkownika. Pliki
+          cookie nie mogą być wykorzystywane do uruchamiania programów lub
+          przenoszenia wirusów na komputer Użytkownika. Pliki cookie są ściśle
+          przypisane do danego użytkownika i mogą być odczytane tylko przez
+          serwer internetowy w domenie, która wygenerowała plik cookie.
+        </p>
+        <p>
+          Pliki cookie mogą być wykorzystywane do zbierania, przechowywania i
+          śledzenia informacji w celach statystycznych lub marketingowych
+          związanych z prowadzeniem naszej Strony internetowej. Użytkownik ma
+          prawo zaakceptować lub odmówić zaakceptowania opcjonalnych plików
+          cookie. Niektóre pliki cookie są wymagane i niezbędne do prawidłowego
+          funkcjonowania Strony internetowej. Takie pliki cookie nie wymagają
+          zgody, ponieważ są aktywne przez cały czas. Pamiętaj , że akceptując
+          wymagane pliki cookie, akceptujesz również pliki cookie stron
+          trzecich, które mogą być wykorzystywane przez inne podmioty podczas
+          korzystania z usług świadczonych przez te podmioty.
+        </p>
+        <h4>Licencja:</h4>
+        <p>
+          O ile nie stwierdzono inaczej, firma INCREMENT POLAND Jarosław
+          Filipiak lub jej licencjodawcy są właścicielami praw własności
+          intelektualnej wszystkich materiałów zamieszczonych na Stronie
+          j-filipiak.pl. Wszelkie prawa własności intelektualnej zostały
+          zastrzeżone. Użytkownicy mogą uzyskać dostęp materiałów dostępnych na
+          Stronie j-filipiak.pl na własny użytek, z zastrzeżeniem ograniczeń
+          określonych w niniejszym regulaminie.
+        </p>
+        <p>Zabrania się:</p>
+        <ul>
+          <li>
+            Kopiowania lub ponownego publikowania materiałów umieszczonych na
+            Stronie j-filipiak.pl
+          </li>
+          <li>
+            Sprzedaży, wypożyczania lub udzielania sublicencji na materiały
+            umieszczone na Stronie j-filipiak.pl
+          </li>
+          <li>
+            Odtwarzania, powielania lub kopiowania materiałów zamieszczonych na
+            Stronie j-filipiak.pl
+          </li>
+          <li>
+            Ponownego rozpowszechniania treści zamieszczonych na Stronie
+            j-filipiak.pl
+          </li>
+        </ul>
+        <p>Niniejsza umowa zaczyna obowiązywać w dniu jej sporządzenia.</p>
+        <p>
+          Użytkownicy mają możliwość zamieszczania i wymiany opinii oraz
+          informacji w niektórych częściach niniejszej Strony internetowej.
+          INCREMENT POLAND Jarosław Filipiak} nie filtruje, nie edytuje, nie
+          publikuje ani nie sprawdza komentarzy przed ich umieszczeniem na
+          Stronie. Komentarze nie odzwierciedlają poglądów i opinii INCREMENT
+          POLAND Jarosław Filipiak, jej przedstawicieli ani podmiotów
+          stowarzyszonych. Zamieszczone komentarze odzwierciedlają poglądy i
+          opinie osoby, która je zamieściła. W zakresie przewidzianym przez
+          obowiązujące prawo INCREMENT POLAND Jarosław Filipiak nie odpowiada za
+          komentarze ani nie ponosi odpowiedzialności za jakiekolwiek
+          zobowiązania, szkody lub wydatki spowodowane lub poniesione w wyniku
+          wykorzystania, zamieszczenia lub pojawienia się komentarzy na
+          niniejszej Stronie.
+        </p>
+        <p>
+          INCREMENT POLAND Jarosław Filipiak zastrzega sobie prawo do
+          monitorowania wszystkich komentarzy i usuwania tych, które można uznać
+          za nieodpowiednie, obraźliwe lub naruszające niniejsze warunki.
+        </p>
+        <p>Użytkownik zapewnia i oświadcza, że:</p>
+        <ul>
+          <li>
+            Jest uprawniony do zamieszczania Komentarzy na naszej Stronie
+            internetowej i posiada wszelkie niezbędne licencje i zgody, aby to
+            robić;
+          </li>
+          <li>
+            Komentarze nie naruszają praw własności intelektualnej, w tym między
+            innymi praw autorskich, patentów lub znaków towarowych stron
+            trzecich;
+          </li>
+          <li>
+            Komentarze nie zawierają treści o charakterze oszczerczym,
+            zniesławiającym, obraźliwym, nieprzyzwoitym lub w inny sposób
+            niezgodnym z prawem, stanowiącym naruszenie prywatności.
+          </li>
+          <li>
+            Komentarze nie będą wykorzystywane do pozyskiwania lub promowania
+            działalności biznesowej lub zwyczajów, ani do prezentowania działań
+            komercyjnych lub niezgodnych z prawem.
+          </li>
+        </ul>
+        <p>
+          Niniejszym Użytkownik udziela INCREMENT POLAND Jarosław Filipiak
+          niewyłącznej licencji na wykorzystywanie, powielanie, edytowanie i
+          upoważnianie innych osób do wykorzystywania, powielania i edytowania
+          jego komentarzy w dowolnej formie i we wszystkich formatach lub
+          mediach.
+        </p>
+        <h4>Zamieszczanie odnośników do naszych treści</h4>
+        <p>
+          Następujące podmioty mogą umieszczać odnośniki do naszych treści bez
+          uprzedniej pisemnej zgody:
+        </p>
+        <ul>
+          <li>Organy rządowe;</li>
+          <li>Wyszukiwarki internetowe;</li>
+          <li>Organizacje prasowe;</li>
+          <li>
+            Podmioty zajmujące się rozpowszechnianiem katalogów online mogą
+            zamieszczać linki do naszej Strony w taki sam sposób, w jaki
+            zamieszczają hiperłącza do stron internetowych innych wymienionych
+            podmiotów; oraz
+          </li>
+          <li>
+            Akredytowane przez system działalności, z wyjątkiem organizacji
+            non-profit, punktów handlowych dla organizacji charytatywnych oraz
+            grup zbierających fundusze na cele charytatywne, które nie mogą
+            zamieszczać hiperłączy do naszej Strony internetowej.
+          </li>
+        </ul>
+        <p>
+          Organizacje te mogą zamieszczać odnośniki do naszej strony głównej,
+          publikacji lub innych materiałów zamieszczonych na Stronie pod
+          warunkiem, że odnośnik ten (a) nie wprowadza w błąd; (b) nie sugeruje,
+          że jest sponsorowany, promowany lub uznany przez stronę linkującą oraz
+          jej produkty i/lub usługi, jeśli nie jest to zgodne z prawdą ; oraz
+          (c) pasuje do kontekstu strony linkującej.
+        </p>
+        <p>
+          Dopuszczamy możliwość rozważenia i zatwierdzenia innych próśb o link
+          od następujących typów organizacji:
+        </p>
+        <ul>
+          <li>
+            powszechnie znanych źródeł informacji dla konsumentów lub podmiotów
+            gospodarczych;
+          </li>
+          <li>działalności internetowych typu dot-com;</li>
+          <li>
+            stowarzyszeń lub innych podmiotów reprezentujących organizacje
+            charytatywne;
+          </li>
+          <li>dystrybutorów katalogów internetowych;</li>
+          <li>portali internetowych;</li>
+          <li>firm księgowych, prawniczych i konsultingowych; oraz</li>
+          <li>instytucji edukacyjnych i stowarzyszeń handlowych.</li>
+        </ul>
+        <p>
+          Zaakceptujemy prośby o link od tych podmiotów, jeśli uznamy, że: (a)
+          link nie będzie nas stawiał w niekorzystnym świetle względem nas
+          samych lub naszych akredytowanych działalności; (b) nie odnotowano u
+          nas żadnych negatywnych zapisów związanych z daną organizacją; (c)
+          korzyść dla nas wynikająca z widoczności hiperłącza rekompensuje brak
+          nazwy INCREMENT POLAND Jarosław Filipiak; oraz (d) link występuje w
+          kontekście informacji ogólnych zasobów.
+        </p>
+        <p>
+          Podmioty te mogą zamieszczać linki do naszej strony głównej pod
+          warunkiem, że link ten (a) nie wprowadza w błąd; (b) fałszywie nie
+          sugeruje sponsorowania, promowania lub zaakceptowania witryny
+          linkującej i jej produktów lub usług; oraz (c) wpisuje się w kontekst
+          witryny linkującej.
+        </p>
+        <p>
+          Jeśli należysz do organizacji wymienionych w punkcie 2 powyżej i
+          jesteś zainteresowany umieszczeniem linku do naszej Strony
+          internetowej, zgłoś nam to wysyłając e-mail na adres INCREMENT POLAND
+          Jarosław Filipiak. W e-mailu należy podać swoje imię i nazwisko, nazwę
+          organizacji / podmiotu, informacje kontaktowe oraz adres URL swojej
+          witryny, listę adresów URL, z których zamierzasz linkować do naszej
+          Strony, a także listę adresów URL naszej witryny, do których chcesz
+          linkować. Nasz czas odpowiedzi wynosi od 2 do 3 tygodni.
+        </p>
+        <p>
+          Zatwierdzone podmioty mogą zamieszczać odnośniki do naszej Strony w
+          następujący sposób:
+        </p>
+        <ul>
+          <li>Używając naszej nazwy (firmy); lub</li>
+          <li>
+            Poprzez zastosowanie ujednoliconego identyfikatora zasobów, do
+            którego prowadzi odnośnik; lub
+          </li>
+          <li>
+            Stosowanie innych opisów naszej Strony internetowej, do której
+            kieruje link, uzasadnionych z punktu widzenia kontekstu i formatu
+            treści strony linkującej.
+          </li>
+        </ul>
+        <p>
+          Nie wyrażamy zgody na wykorzystanie logo lub innych elementów
+          graficznych INCREMENT POLAND Jarosław Filipiak w celu umieszczenia
+          linku bez umowy licencyjnej na wykorzystanie znaku towarowego.
+        </p>
+        <h4>Odpowiedzialność za treść:</h4>
+        <p>
+          Nie ponosimy odpowiedzialności za jakiekolwiek treści, które pojawiają
+          się na innych stronach internetowych. Zobowiązujesz się do zapewnienia
+          nam ochrony przed wszelkimi roszczeniami, które zostaną postawione
+          wobec Twojej Strony. Link(i) nie powinny być umieszczane na Stronie,
+          która może być postrzegana jako oszczercza, nieprzyzwoita lub
+          niezgodna z prawem, lub która w inny sposób łamie, lub zachęca do
+          łamania, prawa osób trzecich.
+        </p>
+        <h4>Zastrzeżenia praw:</h4>
+        <p>
+          Zastrzegamy sobie prawo do zażądania usunięcia wszystkich lub
+          wybranych linków kierujących do naszej Strony. Podmioty linkujące
+          zobowiązują się do natychmiastowego usunięcia wszystkich linków
+          prowadzących do naszej Strony w przypadku otrzymania takiego żądania z
+          naszej strony. Zastrzegamy sobie również prawo do zmiany niniejszych
+          warunków i zasad linkowania w dowolnym momencie. Umieszczanie linków
+          do naszej Strony oznacza akceptację i zobowiązanie do przestrzegania
+          niniejszych zasad i warunków.
+        </p>
+        <h4>Usuwanie linków z naszej Strony</h4>
+        <p>
+          Jeśli zauważysz na naszej Stronie link, który z jakichkolwiek powodów
+          jest obraźliwy, skontaktuj się z nami i poinformuj nas o tym w
+          dowolnym momencie. Rozpatrzymy prośbę o usunięcie linku. Nie
+          zobowiązujemy się jednak do usunięcia takiego linku, ani do udzielenia
+          bezpośredniej odpowiedzi osobie zgłaszającej.
+        </p>
+        <p>
+          Nie gwarantujemy, że informacje zawarte na tej stronie są poprawne.
+          Nie zapewniamy ich kompletności ani dokładności, ani nie zobowiązujemy
+          się do utrzymania dostępności strony lub aktualizacji materiałów na
+          niej zawartych.
+        </p>
+        <h4>Wyłączenie odpowiedzialności:</h4>
+        <p>
+          W maksymalnym zakresie dozwolonym przez obowiązujące prawo, nie
+          udzielamy żadnych poręczeń, gwarancji i innych zobowiązań względem
+          naszej strony internetowej i sposobu jej wykorzystywania. Żadne z
+          postanowień niniejszej klauzuli wyłączenia odpowiedzialności nie
+          będzie:
+        </p>
+        <ul>
+          <li>
+            ograniczać lub wyłączać naszej lub Twojej odpowiedzialności za
+            śmierć lub odniesione obrażenia ciała
+          </li>
+          <li>
+            ograniczać lub wyłączać naszej lub Twojej odpowiedzialność za
+            oszustwa lub świadome wprowadzanie w błąd;
+          </li>
+          <li>
+            ograniczać odpowiedzialności naszej lub Twojej w jakikolwiek sposób,
+            który nie jest zgodny z obowiązującym prawem; lub
+          </li>
+          <li>
+            wyłączać odpowiedzialności naszej lub Twojej, która nie może zostać
+            wyłączona na mocy obowiązującego prawa.
+          </li>
+        </ul>
+        <p>
+          Ograniczenia i wyłączenia odpowiedzialności zawarte w tym ustępie oraz
+          w innych miejscach tego wyłączenia odpowiedzialności: (a) nie
+          naruszają postanowień poprzedniego ustępu; oraz (b) regulują wszystkie
+          zobowiązania wynikające z tego wyłączenia odpowiedzialności, w tym
+          zobowiązania wynikające z umowy, z czynu niedozwolonego oraz z tytułu
+          naruszenia obowiązków ustawowych.
+        </p>
+        <p>
+          Pod warunkiem, że strona internetowa oraz informacje i usługi na niej
+          zawarte są udostępniane nieodpłatnie, nie ponosimy odpowiedzialności
+          za powstanie jakichkolwiek strat lub szkód o dowolnym charakterze.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  useHead({
+    title: 'Projektowanie stron www - tylko profesjonalne strony firmowe',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        title: 'Projektowanie stron www - tylko profesjonalne strony firmowe',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Profesjonalne strony www. 10 lat doświadczenia. Nowoczesne strony zoptymalizowane pod Google. Optymalizacja UX. Poproś o indywidualną wycenę!. Szybki kontakt',
+      },
+
+      { hid: 'og-type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og-title',
+        property: 'og:title',
+        content: 'Projektowanie stron www - tylko profesjonalne strony firmowe',
+      },
+      {
+        hid: 'og-desc',
+        property: 'og:description',
+        content:
+          'Profesjonalne strony www. 10 lat doświadczenia. Nowoczesne strony zoptymalizowane pod Google. Optymalizacja UX. Bezpłatna Wycena - Poproś o indywidualną wycenę!. Szybki kontakt',
+      },
+      {
+        hid: 'og-image',
+        property: 'og:image',
+        content: 'https://j-filipiak.pl/og.jpg',
+      },
+    ],
+    bodyAttrs: {
+      class: 'page-privacy-policy',
+    },
+  });
+</script>
+
+<style lang="scss" scoped>
+  ol {
+    @apply list-decimal;
+  }
+
+  ul {
+    @apply pl-5 pt-2 pb-2;
+  }
+
+  h2,
+  h4 {
+    @apply pt-12 pb-12 text-3xl;
+  }
+
+  p {
+    @apply pb-3;
+  }
+</style>
