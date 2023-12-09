81a2cc412cd3e7c256963109277989097ddba5c3
jaroslaw-filipiak <info@j-filipiak.pl>
2023-12-09 21:09:44 +0100

    add seo tag to privacy policy page

--- pages/polityka-prywatnosci.vue
+++ pages/polityka-prywatnosci.vue
@@ -436,7 +436,45 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+  useHead({
+    title: 'Projektowanie stron www - tylko profesjonalne strony firmowe',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        title: 'Projektowanie stron www - tylko profesjonalne strony firmowe',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Profesjonalne strony www. 10 lat doświadczenia. Nowoczesne strony zoptymalizowane pod Google. Optymalizacja UX. Poproś o indywidualną wycenę!. Szybki kontakt',
+      },
+
+      { hid: 'og-type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og-title',
+        property: 'og:title',
+        content: 'Projektowanie stron www - tylko profesjonalne strony firmowe',
+      },
+      {
+        hid: 'og-desc',
+        property: 'og:description',
+        content:
+          'Profesjonalne strony www. 10 lat doświadczenia. Nowoczesne strony zoptymalizowane pod Google. Optymalizacja UX. Bezpłatna Wycena - Poproś o indywidualną wycenę!. Szybki kontakt',
+      },
+      {
+        hid: 'og-image',
+        property: 'og:image',
+        content: 'https://j-filipiak.pl/og.jpg',
+      },
+    ],
+    bodyAttrs: {
+      class: 'page-privacy-policy',
+    },
+  });
+</script>
 
 <style lang="scss" scoped>
   ol {
