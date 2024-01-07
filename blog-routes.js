62f9310b1afc78c07cbc07efb44b0c9a1fa0ff8a
jaroslaw-filipiak <info@j-filipiak.pl>
2024-01-07 13:14:06 +0100

    handle blog routes

--- blog-routes.js
+++ blog-routes.js
@@ -1,23 +1,27 @@
 import axios from 'axios';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 async function blogRoutes() {
-  const { data } = await axios(
-    'https://www.j-filipiak.pl/api/wp-json/wp/v2/posts?per_page=100'
-  );
+  try {
+    const { data } = await axios(
+      'https://www.j-filipiak.pl/api/wp-json/wp/v2/posts?per_page=100'
+    );
 
-  console.log(data);
+    const prepared = [];
 
-  const prepared = [];
+    data.map((route) => prepared.push('/blog/' + route.title?.rendered));
 
-  data.map((route) => prepared.push('/blog/' + route.title?.rendered));
+    // Write the routes to a file
+    fs.writeFileSync(`${__dirname}/routes.json`, JSON.stringify(prepared));
 
-  // Write the routes to a file
-
-  fs.writeFileSync(`${__dirname}/routes.json`, JSON.stringify(prepared));
-
-  console.log(prepared);
-  return prepared;
+    console.log('Routes generated successfully');
+  } catch (error) {
+    console.error('Error generating routes:', error);
+  }
 }
 
-export default blogRoutes;
+blogRoutes();
